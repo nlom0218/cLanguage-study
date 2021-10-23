cea952e077cbe1f57e410579101afc0b8096286a
nlom0218 <nlom0218@naver.com>
2021-10-23 16:43:46 +0900

    private, public, protected

--- Program.cs
+++ Program.cs
@@ -9,30 +9,7 @@ namespace HelloWord
   class Player // 부모클래스 또는 기반클래스
   // 자식클래스에 공통적으로 가지고 있는 인스턴스, 함수를 넣으면 편하다!
   {
-    static public int count = 1; // 오로지 한개만 존재
-    public int id;
-    public int hp;
-    public int attack;
 
-    public Player()
-    {
-      Console.WriteLine("Player 생성자 호출!");
-    }
-
-    public Player(int hp)
-    {
-      this.hp = hp;
-      Console.WriteLine("player hp 생성자 호출!");
-    }
-    public void Move()
-    {
-      Console.WriteLine("Knight Player");
-    }
-
-    public void Attack()
-    {
-      Console.WriteLine("Knight Player ");
-    }
   }
 
   class Mage : Player // 상속을 받는 자식클래스 또는 파생클래스
@@ -47,8 +24,26 @@ namespace HelloWord
   // 변수를 넘길 때 참조
   class Knigth : Player
   {
+    private int hp;
+    // private : 아무도 접근할 수 없다. 나만 사용할 수 없다.
 
+    public void SetHp(int hp)
+    // public : 모두에게 공유
+    {
+      this.hp = hp;
+    }
 
+    protected int mp;
+    // protected : 외부에서는 사용할 수 없지만 상속을 받은 자식 클래스에서는 사용할 수 있다. 
+  }
+
+  class SuperKnigth : Knigth
+  {
+    void Test()
+    {
+      mp = 10;
+      hp = 20;
+    }
   }
 
   class Program
@@ -56,8 +51,7 @@ namespace HelloWord
     static void Main(string[] args)
     {
       Knigth knigth = new Knigth();
-      Mage mage = new Mage();
-
+      knigth.SetHp(100);
     }
   }
 }
