83e9f56c6504a7c62ba2c6663be84f3e17aac149
nlom0218 <nlom0218@naver.com>
2021-10-25 20:02:46 +0900

    String

--- Program.cs
+++ Program.cs
@@ -55,6 +55,22 @@ namespace HelloWord
       Mage mage = new Mage();
 
       EnterGame(knigth);
+
+      string name = "Harry Potter";
+
+      // 1. 찾기
+      bool found = name.Contains("Harry");
+      int index = name.IndexOf("P");
+
+      // 2. 변형
+      name = name + " Junior";
+      string lowerCaseName = name.ToLower();
+      string upperCaseName = name.ToUpper();
+      string newName = name.Replace("r", "l");
+
+      // 3. 분할
+      string[] names = name.Split(new char[] { ' ' });
+      string substringName = name.Substring(5);
     }
   }
 }
